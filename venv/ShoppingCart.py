76defb1f23f6bb13774d2e4db1366c88174cd0ed
Yaseen <siddiquiyaseen641@gmail.com>
2025-07-15 17:27:03 +0530

    this is python zero to hero logic building folder

---
+++ venv/ShoppingCart.py
@@ -0,0 +1,18 @@
+foods=[]
+prices=[]
+total=0
+
+while(True):
+    food = input("Enter what you want to purchase (q for quit): ")
+    if food=='q':
+        break
+    else:
+        price=float(input(f"Enter price your {food}$: "))
+        foods.append(food)
+        prices.append(price)
+print("_______________You Cart___________")
+for food in foods:
+    print(food,end=" ")
+for price in prices:
+    total+=price
+    print(total)
